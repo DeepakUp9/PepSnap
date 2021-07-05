996dcd1cf4d958a7c47d261220832a0c5649261c
deepak <deepak8588933265.gmail.com>
2021-07-05 13:24:38 +0530

    added video recording Functionlity

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 
 let videoElement = document.querySelector("video")
-let constraint ={ video : true };
+let recordButton =document.querySelector("#record");
+let recordingState =false;
+let mideaRecorder;
 
-navigator.mediaDevices
-.getUserMedia(constraint)
-.then(function (mediaStream){
 
-   // console.log(mediaStream);
-   videoElement.srcObject= mediaStream;
-})
 
-.catch(function (error) {
-     console.log(error);
+(async function () {
+    let constraint ={ video : true };
+
+    let mediaStream = await navigator.mediaDevices.getUserMedia(constraint);
+    videoElement.srcObject= mediaStream;
+
+    mideaRecorder =new MediaRecorder(mediaStream);
+    mideaRecorder.onstart =function () {
+        console.log("insidem on start");
+        
+    };
+
+    mideaRecorder.ondataavailable = function (e){
+        console.log("inside on data available");
+    console.log(e.data);
+    let videObject = new Blob ([e.data], {type :"video/mp4" });
+    console.log(videObject);
+};
+
+mideaRecorder.onstop =function(){
+    console.log("inside on stop");
+};
+
+
+recordButton.addEventListener("click",function(){
+          if(recordingState){
+              //already recording is going on 
+              //stop the recording 
+              mideaRecorder.stop();
+              recordButton.innerHTML="Record Video";
+              recordingState =false;
+          }
+          else{
+                  // start the video recording 
+                  mideaRecorder.start();
+                  recordButton.innerHTML="Recording....";
+                  recordingState =true;
+          }
 });
+
+})();
