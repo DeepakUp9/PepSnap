5cda2107c3b841f64ac3fa95621f8f768d7f15df
deepak <deepak8588933265.gmail.com>
2021-07-05 14:35:02 +0530

    added video recording button and caputure button

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ let recordButton =document.querySelector("#record");
 let recordingState =false;
 let mideaRecorder;
 
+let downloadButton =document.querySelector("#download");
+let capturePhoto =document.querySelector("#capture");
+
+
+
 
 
 (async function () {
-    let constraint ={ video : true };
+    // let constraint ={ video : true ,  audio:true };
+    let constraint ={ video : true  };
 
     let mediaStream = await navigator.mediaDevices.getUserMedia(constraint);
     videoElement.srcObject= mediaStream;
@@ -20,9 +26,22 @@ let mideaRecorder;
 
     mideaRecorder.ondataavailable = function (e){
         console.log("inside on data available");
+
     console.log(e.data);
-    let videObject = new Blob ([e.data], {type :"video/mp4" });
+     videObject = new Blob ([e.data], {type :"video/mp4" });
     console.log(videObject);
+
+    let VideioUrl = URL.createObjectURL(videObject);
+
+    let aTag =document.createElement("a");
+
+    aTag.download =`Video${Date.now()}.mp4`;
+    aTag.href =VideioUrl;
+
+    downloadButton.addEventListener("click",function(){
+        aTag.click();
+    })
+  
 };
 
 mideaRecorder.onstop =function(){
@@ -45,5 +64,27 @@ recordButton.addEventListener("click",function(){
                   recordingState =true;
           }
 });
+
+
+capturePhoto.addEventListener("click",function(){
+    //canvas 
+    let canvas =document.createElement("canvas");
+    canvas.width = 640;   //video widht
+    canvas.height =480  // video height
+
+
+    let ctx =canvas.getContext("2d");
+
+    ctx.drawImage(videoElement,0,0);
+
+    //download canvas as an image 
+    let aTag =document.createElement("a");
+    aTag.download =`image${Date.now()}.jpg`;
+    aTag.href = canvas.toDataURL("image/jpg");
+    aTag.click();
+
+});
+
+
 
 })();
